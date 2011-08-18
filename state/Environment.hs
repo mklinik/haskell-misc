d6982f0b9e4cf6c66d2490edf485d72c465d0030
Markus Klinik <markus.klinik@gmx.de>
2011-08-18 13:38:11 +0200

    simon says: and those are monads

--- state/Environment.hs
+++ state/Environment.hs
@@ -1,8 +1,30 @@
 module Environment where
 
+import Data.Maybe
+
 type Env = [(String, Int)]
+newtype EnvM a = EnvM { runEnvM :: Env -> (a, Env) }
+
+instance Monad EnvM where
+  (EnvM f) >>= g  = EnvM (\s -> let (a, s_) = f s in runEnvM (g a) s_)
+  return x = EnvM $ \s -> (x, s)
+
+(>>>=) :: (Env -> (Int, Env)) -> (Int -> Env -> (Int, Env)) -> (Env -> (Int, Env))
+f >>>= g = \s -> let (x, s1) = f s in g x s1
+
+(>>>) :: (Env -> (a, Env)) -> (Env -> (b, Env)) -> (Env -> (b, Env))
+f >>>  g = \s -> let (_, s1) = f s in g   s1
+
+myReturn :: Int -> Env -> (Int, Env)
+myReturn x = \s -> (x, s)
 
 emptyEnv = []
+
+assignM :: String -> Int -> EnvM ()
+assignM name val = EnvM (\s -> ((), (name, val) : s))
+
+readM :: String -> EnvM Int
+readM name = EnvM (\s -> (fromMaybe 0 $ lookup name s, s))
 
 assign :: String -> Int -> Env -> ((), Env)
 assign name val env = ((), (name, val):env)
@@ -15,12 +37,12 @@ readOut name env = readOut_ name env env
       | n == name = (value, env)
       | otherwise = readOut_ n rest env
 
-flup =
-  assign "foo" 100 >>>
-  assign "bar" 42 >>>
-  readOut "foo" >>>= \x ->
-  readOut "bar" >>>= \y ->
-  myReturn (x+y)
+-- flup =
+  -- assign "foo" 100 >>>
+  -- assign "bar" 42 >>>
+  -- readOut "foo" >>>= \x ->
+  -- readOut "bar" >>>= \y ->
+  -- myReturn (x+y)
 
 foo = assign "foo" 100
 
@@ -35,27 +57,15 @@ bar = assign "foo" 100 >>> readOut "foo"
   -- return (x+y)
 
 
--- blah = do
-  -- assign "foo" 100
-  -- assign "bar" 42
-  -- x <- readOut "foo"
-  -- y <- readOut "bar"
-  -- return (x+y)
+blah = do
+  assignM "foo" 100
+  assignM "bar" 42
+  x <- readM "foo"
+  y <- readM "bar"
+  return (x+y)
 
-(>>>=) :: (Env -> (Int, Env)) -> (Int -> Env -> (Int, Env)) -> (Env -> (Int, Env))
-f >>>= g = \s -> let (x, s1) = f s in g x s1
-
-(>>>) :: (Env -> (a, Env)) -> (Env -> (b, Env)) -> (Env -> (b, Env))
-f >>>  g = \s -> let (_, s1) = f s in g   s1
-
-myReturn :: Int -> Env -> (Int, Env)
-myReturn x = \s -> (x, s)
-
--- instance Monad ??? where
-  -- (>>=) = (>>>=)
-  -- (>>) = (>>>)
-  -- return = myReturn
-
+run :: EnvM a -> Env -> a
+run f e = fst $ (runEnvM f) e
 
 runWithEnv :: Env -> (Env -> (a, Env)) -> a
 runWithEnv e f = let (x, _) = f e in x
