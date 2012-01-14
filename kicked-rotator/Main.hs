2af30deb71f3c6d043f7306e786d9af1e3368bcb
Markus Klinik <markus.klinik@gmx.de>
2012-01-14 18:15:38 +0100

    kicked-rotator: cleanup

--- kicked-rotator/Main.hs
+++ kicked-rotator/Main.hs
@@ -3,7 +3,6 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified System.Random as Random
 import Control.Monad
-import System.IO
 
 data RGB = RGB Int Int Int
 
@@ -14,7 +13,7 @@ kickedRotator k (pn, xn) = (pn, xn) : (kickedRotator k (pnp1 `mod'` pi2, xnp1 `m
     pnp1 = pn + k * (sin xn)
     xnp1 = (xn + pnp1)
 
--- take one data point in the range (-2*pi, 2*pi)
+-- take one data point in the range [-2*pi, 2*pi]
 -- and scale it to the image with and height
 scale :: Int -> Int -> (Double, Double) -> (Int, Int)
 scale width height (p, x) = (pScaled, xScaled)
@@ -36,7 +35,6 @@ map2ppm m width height =
   "255\n" ++
   (foldr (++) "" [ printPixel m x y | x <- [0..(width - 1)], y <- [0..(height - 1)] ])
 
--- an infinite list of random colors
 randomColor :: IO RGB
 randomColor = do
   r <- liftM (\x -> x `mod` 256) Random.randomIO
@@ -44,27 +42,29 @@ randomColor = do
   b <- liftM (\x -> x `mod` 256) Random.randomIO
   return $ RGB r g b
 
+-- one random point (x, y) in the interval [-pi * 2, pi * 2]
 randomPoint :: IO (Double, Double)
 randomPoint = do
   x <- liftM (\x -> x * pi2) Random.randomIO
   y <- liftM (\x -> x * pi2) Random.randomIO
   return (x, y)
 
-
+-- insert iterations number of pixels of the given color in a map
 insertPixels :: Int -> RGB -> Map (Int, Int) RGB -> [(Int, Int)] -> Map (Int, Int) RGB
 insertPixels iterations color m pixels = foldl (\map pixel -> Map.insert pixel color map) m (take iterations $ pixels)
 
 main = do
-  let iterations = 10000
-  let k = 0.971635
+  let iterations = 1000
+  -- let k = 0.971635
+  -- let k = 0.9
+  let k = 0.5
   let width = 600
   let height = 600
-  startPoints <- replicateM 6 randomPoint
-  hPutStr stderr $ show startPoints
+  startPoints <- replicateM 80 randomPoint
   let lines = map (map (scale width height)) $ map (kickedRotator k) startPoints
-  foo <- foldM (\m pixels -> do
-                   color <- randomColor
-                   return $ insertPixels iterations color m pixels
-                  ) Map.empty lines
-  putStr $ map2ppm foo width height
-  putStr ""
+  pixelMap <- foldM
+    (\m pixels -> do
+                  color <- randomColor
+                  return $ insertPixels iterations color m pixels
+    ) Map.empty lines
+  putStr $ map2ppm pixelMap width height
