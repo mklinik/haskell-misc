bc294d01bdc0a730e55d1e6163924b80bbb93d44
Markus Klinik <markus.klinik@gmx.de>
2012-08-25 15:26:15 +0200

    gpxStats: add options -h and -s to switch between modes

--- gpxStatistics/Main.hs
+++ gpxStatistics/Main.hs
@@ -2,6 +2,36 @@ module Main where
 
 import           Util
 import           System.Environment (getArgs)
+import           System.Console.GetOpt
+
+data Options = Options
+  { mode :: (FilePath -> IO ())
+  }
+  deriving (Show)
+
+defaultOptions :: Options
+defaultOptions = Options
+  { mode = printGPXStatistics
+  }
+
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option ['h'] ["histogram"]
+      (NoArg (\o -> o { mode = printGPXStatistics }))
+      "print an ASCII histogram (default)"
+  , Option ['s'] ["speed"]
+      (NoArg (\o -> o { mode = printGPXSpeeds }))
+      "print a list of speed values"
+  ]
+
+mkOptions :: [String] -> IO (Options, [String])
+mkOptions argv =
+  case getOpt Permute options argv of
+    (o, n, []  ) -> return (foldl (flip id) defaultOptions o, n)
+    (_, _, errs) -> ioError (userError (concat errs ++ usageInfo header options))
+  where header = "gpxStatistics [OPTION...] files..."
 
 main :: IO ()
-main = getArgs >>= mapM_ printGPXStatistics
+main = do
+  (opts, args) <- getArgs >>= mkOptions
+  mapM_ (mode opts) args
