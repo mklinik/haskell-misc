906a5b6c140960957bc2b1ebe0b630a071f8d566
Markus Klinik <markus.klinik@gmx.de>
2012-12-02 11:02:01 +0100

    HelloCairo: button and clean exit

--- cairo/HelloCairo.hs
+++ cairo/HelloCairo.hs
@@ -2,8 +2,15 @@ module Main where
 
 import Graphics.UI.Gtk
 
+hello :: (ButtonClass b) => b -> IO ()
+hello b = set b [buttonLabel := "Hello World"]
+
 main = do
   initGUI
   window <- windowNew
+  button <- buttonNew
+  set window [containerChild := button, containerBorderWidth := 10]
+  onClicked button (hello button)
+  onDestroy window mainQuit
   widgetShowAll window
   mainGUI
